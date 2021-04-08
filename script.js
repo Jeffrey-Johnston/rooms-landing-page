84770fe8823020cc219e5fb8413534735eff1d01
Jeffrey Johnston <JJohnston1822@gmail.com>
2021-04-08 14:50:43 -0400

    initial-commit

---
+++ script.js
@@ -0,0 +1,30 @@
+"use strict"
+
+let navbar = document.querySelector('.navbar')
+let hamburger = document.querySelector('.hamburger')
+let closeNav = document.querySelector('.close-nav')
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
+
+hamburger.addEventListener("click", () => {
+  navbar.style.display = "flex";
+});
+closeNav.addEventListener("click", () => {
+  navbar.style.display = "none";
+});
